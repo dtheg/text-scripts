350f098bd7e741c148e30b475310b0e5372eaafa
leopoldo garay <leodgaray@MacBook-Pro-2.local>
2021-09-16 23:27:36 -0700

    trying with a function

--- sound_sched2.py
+++ sound_sched2.py
@@ -9,33 +9,42 @@ bros = ['Jonah','David','Johnny','Jonathan', 'Edgar', 'Ricky']
 
 # Yield successive n-sized
 # chunks from l.
-def divide_chunks(l, n):
+# def divide_chunks(l, n):
 
-# looping till length l
-	for i in range(0, len(l), n): 
-		yield l[i:i + n]
+# # # looping till length l
+# # 	for i in range(0, len(l), n): 
+# # 		yield l[i:i + n]
 
-# How many elements each
-# list should have
-n = 3
 
-bros_chunked = list(divide_chunks(bros, n))
+
+def chunks(l, n):
+#Yield n number of sequential chunks from l."""
+	d, r = divmod(len(l), n)
+	for i in range(n):
+		si = (d+1)*(i if i < r else r) + d*(0 if i < r else i - r)
+		yield l[si:si+(d+1 if i < r else d)]
+
+
+# # How many elements each
+# # list should have
+# n = 3
+
+# bros_chunked = list(divide_chunks(bros, n))
 
 # for i in range(len(bros_chunked)):
 # 	for x in bros_chunked:
 #         print(x[i], end =' ')
 
-
-bros_shuffled = random.choice(bros_chunked)
+#bros_shuffled = random.shuffle(bros)
 
 # def assign():
 
-df['Name'] = [bros_shuffled for Name in df.Name]
-# assign()
+#df['Name'] = [random.choice(bros) for i in df.Name]
+sched = chunks(df.Name, 31)
 
 
-print(df)
+print(sched)
 
-#df.to_csv('/Users/leodgaray/Documents/Zoom Assignment Schedule.csv', index=False)
+#df.to_csv('/Users/leodgaray/Documents/congregation/Zoom Assignment Schedule.csv', index=False)
 
 #print('Schedule created')  
