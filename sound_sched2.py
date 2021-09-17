3f413555eb19e4160800693693cce9d7587316d2
leopoldo garay <leodgaray@MacBook-Pro-2.local>
2021-09-16 23:31:00 -0700

    names are randomized, not in chunks. smh

--- sound_sched2.py
+++ sound_sched2.py
@@ -7,43 +7,11 @@ import random
 df = pd.DataFrame(index=np.arange(31), columns= ['Date', 'Name', 'Media', 'Muting', 'Host', 'CoHost', 'Attendant'])
 bros = ['Jonah','David','Johnny','Jonathan', 'Edgar', 'Ricky']
 
-# Yield successive n-sized
-# chunks from l.
-# def divide_chunks(l, n):
 
-# # # looping till length l
-# # 	for i in range(0, len(l), n): 
-# # 		yield l[i:i + n]
+df['Name'] = [random.choice(bros) for i in df.Name]
 
 
-
-def chunks(l, n):
-#Yield n number of sequential chunks from l."""
-	d, r = divmod(len(l), n)
-	for i in range(n):
-		si = (d+1)*(i if i < r else r) + d*(0 if i < r else i - r)
-		yield l[si:si+(d+1 if i < r else d)]
-
-
-# # How many elements each
-# # list should have
-# n = 3
-
-# bros_chunked = list(divide_chunks(bros, n))
-
-# for i in range(len(bros_chunked)):
-# 	for x in bros_chunked:
-#         print(x[i], end =' ')
-
-#bros_shuffled = random.shuffle(bros)
-
-# def assign():
-
-#df['Name'] = [random.choice(bros) for i in df.Name]
-sched = chunks(df.Name, 31)
-
-
-print(sched)
+print(df)
 
 #df.to_csv('/Users/leodgaray/Documents/congregation/Zoom Assignment Schedule.csv', index=False)
 
